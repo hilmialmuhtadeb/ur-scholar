ce14c6979a56793c4296d97e231a7939f3606ceb
firdanisa1415 <firdanisa1415@gmail.com>
2022-11-09 10:37:34 +0700

    data baru

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -1,40 +1,20 @@
 <x-layout>
-  <div id="carouselExampleCaptions" class="carousel slide home-carousel" data-bs-ride="false">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <div class="container">
+<div class="row mt-3">
+    <div class="col">
+        <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+                <h2>Layanan Pengaduan Permasalahan Sistem</h2>
+                <p class="mt-4">Laporkan dan ajukan Terkait permasalahan 
+                    sistem operasional anda disini, 
+                    Operator kami akan segera memprosesnya</p>
+                <button type="button" class="btn btn-lg mt-3 text-white" style="background-color: #0E73B9;">Ajukan Laporan</button>
+            </div>
+        </div>
     </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="https://images.unsplash.com/photo-1595134561159-11a5fbf6b3f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2Nob2xhcnNoaXB8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>First slide label</h5>
-          <p>Some representative placeholder content for the first slide.</p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="https://images.unsplash.com/photo-1595134561159-11a5fbf6b3f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2Nob2xhcnNoaXB8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
-          <p>Some representative placeholder content for the second slide.</p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="https://images.unsplash.com/photo-1595134561159-11a5fbf6b3f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2Nob2xhcnNoaXB8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Third slide label</h5>
-          <p>Some representative placeholder content for the third slide.</p>
-        </div>
-      </div>
+    <div class="col">
+        <img src={{ asset('svg/Hero1.png') }} alt="" width="100%">
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
+  </div>
   </div>
 </x-layout>
