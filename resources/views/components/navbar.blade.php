fc6b1541fc3c6df184699803968080bec82a8139
firdanisa1415 <99249653+firdanisa1415@users.noreply.github.com>
2022-11-09 14:36:47 +0700

    Revert "index"

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,8 +1,6 @@
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg bg-primary">
   <div class="container">
-  <a class="navbar-brand p-2" href="#">
-  <img src={{ asset('svg/Brandu.png') }} alt="Brand image" style="height: 50px;"> UrScholar
-</a>
+  <img src={{ asset('svg/Group_5.png') }} alt="Brand image" style="height: 30px;">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -3,15 +3,12 @@
 <div class="row mt-3">
     <div class="col">
         <div class="d-flex h-100">
-            <div class="justify-content-center align-self-center mt-3">
-                <h2>Sistem Informasi Beasiswa</h2>
-                <p class="mt-4">UrScholar merupakan wadah bagi 
-                    masyarakat Indonesia yang ingin mengembangakan karier, pengalaman, 
-                    dan jaringan kampus di dalam dan luar negeri. 
-                    Melalui beberapa informasi yang ditawarkan oleh UrScholar,
-                    UrScholar berkomitmen untuk menyiapkan informasi beasiswa yang terbaru dan aman untuk 
-                    salah satu bentuk kontribusi dalam proses memajukan Indonesia. Maka Tunggu apa lagi, Segera Cari dan Daftarkan dirimu!!! </p>
-                <button type="button" class="btn btn-lg mt-3 text-white" style="background-color: #0E73B9;">Cari Beasiswa</button>
+            <div class="justify-content-center align-self-center">
+                <h2>Layanan Pengaduan Permasalahan Sistem</h2>
+                <p class="mt-4">Laporkan dan ajukan Terkait permasalahan 
+                    sistem operasional anda disini, 
+                    Operator kami akan segera memprosesnya</p>
+                <button type="button" class="btn btn-lg mt-3 text-white" style="background-color: #0E73B9;">Ajukan Laporan</button>
             </div>
         </div>
     </div>
@@ -19,37 +16,5 @@
         <img src={{ asset('svg/Hero1.png') }} alt="" width="100%">
     </div>
   </div>
-  <div class="container">
-      <h3 class="text-center mt-5 mb-4 py-2 border-bottom">Jenis Beasiswa</h3>
-      <div class="row">
-        <div class="col-sm-4">
-          <div class="card text-center" style="height: 19rem;">
-            <div class="card-body">
-              <h6 class="card-title mr-2 fs-5 text-center">SMP</h6>
-              <img src={{ asset('svg/smp.png') }} alt="" width="30%"/>
-              <p class="card-text fs-6 text-center">Isikan data diri pelapor berupa Username dan Password yang Terdaftar</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-4">
-          <div class="card text-center" style="height: 19rem;">
-            <div class="card-body">
-              <h6 class="card-title mr-2 fs-5 text-center">SMA</h6>
-              <img src={{ asset('svg/sma2.png') }} alt="" width="28%"/>
-              <p class="card-text fs-6 text-center">Isikan data diri pelapor berupa Username dan Password yang Terdaftar</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-4">
-          <div class="card text-center" style="height: 19rem;">
-            <div class="card-body">
-              <h6 class="card-title mr-2 fs-5 text-center">Kuliah</h6>
-              <img src={{ asset('svg/kuliah.png') }} alt="" width="20%"/>
-              <p class="card-text fs-6 text-center">Kirim data. Data akan diproses oleh operator kami</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </x-layout>
